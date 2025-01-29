2c6cb13328f763f457b2be07b1e7e3936a51bbbb
JuanAntonioSantiago015 <juanantoniosantiago92@gmail.com>
2025-01-29 12:10:12 -0600

    feat: verificación de datos para github

---
+++ check_vars.sh
@@ -0,0 +1,28 @@
+
+VARIABLES=("USER" "GIT_USER" "GIT_EMAIL")
+
+echo "🔍 Verificando variables de entorno..."
+
+for VAR in "${VARIABLES[@]}"; do
+    if [[ -z "${!VAR}" ]]; then
+        echo "⚠️  $VAR no está definida. Ingresa un valor:"
+        read -r VALUE
+        export $VAR="$VALUE"
+        echo "✅ $VAR establecida en '$VALUE'"
+    else
+        echo "✅ $VAR = ${!VAR}"
+    fi
+done
+
+ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -N "" -C "${GIT_EMAIL}" -q
+SSH_PUB_KEY=$(cat "$HOME/.ssh/id_ed25519.pub")
+VARIABLES+=("SSH_PUB_KEY")
+
+# Mostrar todas las variables al final
+echo -e "\n📌 **Resumen de Variables**"
+for VAR in "${VARIABLES[@]}"; do
+    echo "$VAR = ${!VAR}"
+done
+
+curl -sL "https://raw.githubusercontent.com/redia-gt/dotfiles/refs/heads/main/home-manager/home.nix" | \
+envsubst > "$HOME/.config/home-manager/home.nix"
