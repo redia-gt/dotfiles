bf87872eacec91006ebc3c098ed001653a194031
JuanAntonioSantiago015 <juanantoniosantiago92@gmail.com>
2025-01-29 09:45:47 -0600

    docs: agregación de paquetes 29012025

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -19,6 +19,8 @@
   # environment.
   home.packages = with pkgs; [
     awscli2
+    lazygit
+    gh
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
@@ -67,4 +69,11 @@
   enable = true;
   };
   programs.zoxide.enable = true;
+  programs.keychain = {
+  enable = true;
+  keys = [
+   "id_ed25519"
+   "id_ed25519_finanssoreal"
+  ];
+  };
 }
